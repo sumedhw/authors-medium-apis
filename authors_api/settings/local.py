e7ea6b0f959870cd69def208d6c1e41e07bfea6d
Asawari Takpire <asawaritakpire16@gmail.com>
2024-04-07 16:09:26 +0530

    setup basic django project

---
+++ authors_api/settings/local.py
@@ -0,0 +1,20 @@
+from .base import *  # noqa
+from .base import env
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="oXPWQPA3C3sdBCuBeXUKq3LBp9YDJ33-306p9EAKf1ja1xkWnKY",
+)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
+
+# EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+# EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+# EMAIL_PORT = env("EMAIL_PORT")
+# DEFAULT_FROM_EMAIL = "support@apiimperfect.site"
+# DOMAIN = env("DOMAIN")
+# SITE_NAME = "Authors Api"
