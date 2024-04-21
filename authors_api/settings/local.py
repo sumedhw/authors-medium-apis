cb242ee1811da19b88740133e8240fb2437dab7b
Sumedh Waghmare <sumedhw71@gmail.com>
2024-04-22 00:13:46 +0530

    configure celery redis flower

--- authors_api/settings/local.py
+++ authors_api/settings/local.py
@@ -12,9 +12,9 @@ DEBUG = True
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
 
-# EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
-# EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
-# EMAIL_PORT = env("EMAIL_PORT")
-# DEFAULT_FROM_EMAIL = "support@apiimperfect.site"
-# DOMAIN = env("DOMAIN")
-# SITE_NAME = "Authors Api"
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+EMAIL_PORT = env("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = "support@apiimperfect.site"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Authors Api"
